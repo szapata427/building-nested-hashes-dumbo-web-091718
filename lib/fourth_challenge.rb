392f51104ab597c8f7670276f7b9d64c6200b455
Steven Zapata <szapata427@gmail.com>
2018-09-08 20:45:10 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,7 +5,7 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-      {friend: "1", name: "Benvolio", age: "17", attitude: "worried"}, {friend: "2", name: "Mercutio", attitude: "hot-headed"
+      {name: "Benvolio", age: "17", attitude: "worried"}, {name: "Mercutio", attitude: "hot-headed"
         
       }
       ]
