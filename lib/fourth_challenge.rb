530d862d7ddbd9fc2489d4cd4089eb442b3b40cf
Steven Zapata <szapata427@gmail.com>
2018-09-08 20:42:44 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -16,7 +16,9 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [ {name: "Steven",
         age: "30",
-        attitude: "confused"}, 
+        attitude: "confused"}, { name: "Nurse",
+        age: "44",
+        attitude: "worried"}
         
       ]
    }
