e7a00b1dac9f099f73a4d0e5a5e3e2b8d6cb706f
Steven Zapata <szapata427@gmail.com>
2018-09-08 20:52:23 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,10 +1,13 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+    :patriarch => {}
+    :hero => {}
+    :hero_fiends => {}
       }, 
     :capulet => {
-      #your key/value pairs here!
+      
+      
       }}
 
   
